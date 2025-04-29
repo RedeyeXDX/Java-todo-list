c111576442585345c9e6a9c5b3c85fbbbef312c4
RedeyeXDX <mrbrandonpeh@gmail.com>
2025-04-29 22:11:03 +0800

    added Abstract class

--- src/BaseTask.java
+++ src/BaseTask.java
@@ -1,4 +1,37 @@
-public class BaseTask {
+public abstract class BaseTask {
     protected int id;
+    protected String description;
+    protected boolean isCompleted;
 
+    public BaseTask(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public abstract String getDetail();
+
+    public void markAsCompleted() {
+        this.isCompleted = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.isCompleted = completed;
+    }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,35 +1,11 @@
-public class Task {
-    private int id;
-    private String description;
-    private boolean isCompleted;
+public class Task extends BaseTask {
 
-    public Task (int id, String description){
-        this.id = id;
-        this.description = description;
-        this.isCompleted = false;
+    public Task(int id, String description) {
+        super(id, description); // use parent constructor
     }
 
-    public void markAsCompleted(){
-        isCompleted = true;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isCompleted() {
-        return isCompleted;
-    }
-
-    public void setCompleted(boolean completed) {
-        isCompleted = completed;
+    public String getDetail() {
+        String status = isCompleted ? "[Completed]" : "[Pending]";
+        return id + ". " + description + " " + status;
     }
 }
